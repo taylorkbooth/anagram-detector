0eb2666aadf537fefc007c77830f20efc4837b8b
Taylor Booth <taylorkbooth@gmail.com>
2020-11-08 18:59:41 -0500

    commit tree ec0789077399bce17e14169ccee5bc493bf01010

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+    @word = word
+end
+
+def match(array)
+    new_array= []
+    return_array = []
+    array.each do |item|
+        new_array = item.split("")
+        if new_array.sort == @word.split("").sort
+            return_array << item
+        end
+    end
+    return_array
+end
+# binding.pry
+end
